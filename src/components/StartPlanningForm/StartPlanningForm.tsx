5dbcff049b90eee8e1da8cebf630df67169a4123
Emma Tran <emmayentran@gmail.com>
2022-02-11 00:19:53 -0800

    tailwind pathing fix and component resized

--- src/components/StartPlanningForm/StartPlanningForm.tsx
+++ src/components/StartPlanningForm/StartPlanningForm.tsx
@@ -17,56 +17,57 @@ interface PlanningFormValues {
 const StartPlanningForm: React.FC<unknown> = () => {
   const [startDate, setStartDate] = useState<Date | null | undefined>();
   const [endDate, setEndDate] = useState<Date | null | undefined>();
-
   const initialValues: PlanningFormValues = {
     whereTo: "",
   };
   return (
-    <div className="border border-radiusOrange">
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values, actions) => {
-          console.log({ values, actions });
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }}
-      >
-        <Form className='flex flex-col w-96'>
-          <div className='flex mb-12 w-full'>
-            <label className='font-semibold text-2xl ml-4'>Where to?</label>
-            <Field
-              id='whereTo'
-              name='whereTo'
-              placeholder='e.g. Seattle, Los Angeles, Portland'
-              className='rounded bg-offWhite'
-            />
-          </div>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values, actions) => {
+        console.log({ values, actions })
+        alert(JSON.stringify(values, null, 2))
+        actions.setSubmitting(false)
+      }}
+    >
+      <Form className="flex flex-col w-[30rem]">
+        <div className="flex items-center h-16 border rounded border-inputGrey mb-2 pl-4">
+          <label className="font-semibold text-xl w-3/12">Where to?</label>
+          <Field
+            id="whereTo"
+            name="whereTo"
+            placeholder="e.g. Seattle, Los Angeles, Portland"
+            className="rounded bg-offWhite w-9/12 h-full"
+          />
+        </div>
 
-          <div className='flex flex-col border rounded h-16 w-full mb-8 pl-4'>
-            <label className='text-sm font-semibold'>Dates (optional)</label>
-            <div className='flex flex-row mx-4 mt-auto'>
-              <div className='flex items-center mb-2'>
-                <BiCalendar className='mr-4 text-2xl' />
-                <DatePicker
-                  className='bg-offWhite w-32'
-                  selected={startDate}
-                  onChange={(date) => setStartDate(date)}
-                />
-              </div>
-              <div className='flex items-center mt-auto mb-2'>
-                <BiCalendar className='mr-2 text-2xl' />
-                <DatePicker
-                  className='bg-offWhite w-32'
-                  selected={endDate}
-                  onChange={(date) => setEndDate(date)}
-                />
-              </div>
+        <div className="flex flex-col h-16 w-full mb-4 border rounded border-inputGrey mt-2 pl-4 pt-2">
+          <label className="text-sm font-semibold">Dates (optional)</label>
+          <div className="flex mt-auto">
+            <div className="flex items-center mb-2">
+              <BiCalendar className="mr-2 text-2xl" />
+              <DatePicker
+                className="bg-offWhite w-32 outline-0"
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                minDate={new Date()}
+                placeholderText='Start date'
+              />
+            </div>
+            <div className="flex items-center mt-auto mb-2">
+              <BiCalendar className="mr-2 text-2xl" />
+              <DatePicker
+                className="bg-offWhite w-32 outline-0"
+                selected={endDate}
+                onChange={(date) => setEndDate(date)}
+                minDate={new Date()}
+                placeholderText='End date'
+              />
             </div>
           </div>
-        </Form>
-      </Formik>
-    </div>
-  );
+        </div>
+      </Form>
+    </Formik>
+  )
 };
 
 export default StartPlanningForm;
